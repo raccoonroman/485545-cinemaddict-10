8659fae8c99d96585a2aa932aa0d47728b1e2a2c
Роман <kromanjs@gmail.com>
2020-01-05 15:37:32 +0200

    8.1 Render other data from server

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,7 +1,6 @@
 export default class Movie {
   constructor(data) {
     this.id = data[`id`];
-    this.comments = data[`comments`];
     this.filmInfo = {};
     this.filmInfo.title = data[`film_info`][`title`];
     this.filmInfo.alternativeTitle = data[`film_info`][`alternative_title`];
@@ -16,8 +15,11 @@ export default class Movie {
     this.filmInfo.duration = data[`film_info`][`runtime`];
     this.filmInfo.genres = data[`film_info`][`genre`];
     this.filmInfo.description = data[`film_info`][`description`];
-
-    // this.comments = [];
+    this.userRating = data[`user_details`][`personal_rating`];
+    this.isInWatchlist = data[`user_details`][`watchlist`];
+    this.isWatched = data[`user_details`][`already_watched`];
+    this.watchingDate = data[`user_details`][`watching_date`];
+    this.isFavorite = data[`user_details`][`favorite`];
   }
 
   // toRAW() {
